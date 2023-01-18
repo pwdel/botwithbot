d03a5e9af9a2bbdafdc4559ffd56ed15d6ab2567
Patrick Delaney <patrick.delaney@bloomboard.com>
2023-01-18 17:32:50 -0600

    Working chatbot, callable by CherryPy

--- noversions.Dockerfile
+++ noversions.Dockerfile
@@ -1,29 +1,54 @@
-FROM python:3.8-slim-buster
+FROM ubuntu:22.04
+ARG DEBIAN_FRONTEND=noninteractive
+
+# Update default packages
+RUN apt-get update && apt-get install -y \
+    build-essential \
+    curl
+
+RUN curl -y --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # Install TensorFlow and system dependencies
 RUN apt-get update && apt-get install -y \
         build-essential \
+        curl \
         libhdf5-dev \
+        libblas-dev \
         libc-ares-dev \
+        libcairo2-dev \
         libeigen3-dev \
         libatlas-base-dev \
         libopenblas-dev \
-        libblas-dev \
+        libpango1.0-dev \
         liblapack-dev \
+        libssl-dev \
         gfortran \
         python3-dev \
         python3-pip \
+        pip \
+        binutils \
+        gcc \
+        libc-dev \
+        libc6 \
+        # libgcc-s1 \
+        libstd-rust-dev \
+        cargo \
+        rustc \
         wget \
         unzip
 
-RUN pip3 install tensorflow==2.2
+
+# Upgrade Pip
+RUN pip install --upgrade pip
 
 # Install packages from requirements.txt
 COPY requirements.txt .
-RUN pip3 install -r requirements.txt
+RUN pip install -r requirements.txt
 
 # Copy code from development container
-COPY ./volumebindmount .
+COPY ./volumebindmount ./home
 
+# Set the working directory to /home
+WORKDIR /home
 # Set the entry point
-CMD python app.py
+CMD /bin/python3.10 app.py
--- volumebindmount/app.py
+++ volumebindmount/app.py
@@ -1,9 +1,56 @@
 import cherrypy
+import transformers
+from transformers import GPT2Tokenizer, TFGPT2LMHeadModel
 
-class HelloWorld:
+class SayWords:
     @cherrypy.expose
-    def index(self):
-        return "Hello, World!"
+    def index(self, words=None):
+
+        def do_tokenization_from_gpt2(words_list):
+
+            tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+            model = TFGPT2LMHeadModel.from_pretrained("gpt2") # remove pad_token_id=tokenizer.eos_token_id
+
+            prompt = " ".join(words_list)
+            print("prompt type",type(prompt))
+            input_ids = tokenizer.encode(prompt, return_tensors="tf")
+            print("input_ids: ", input_ids)
+
+            # generate up to 30 tokens
+            beam_output = model.generate(
+                input_ids, 
+                max_length=50,
+                num_beams=5, 
+                no_repeat_ngram_size=4, 
+                early_stopping=True
+            )
+
+            print("beam_output",beam_output)
+            print("beam_output",type(beam_output))
+
+            sentence_return = tokenizer.decode(beam_output[0])
+            print("type sentence_return: ",type(sentence_return))
+            print("sentence_return: ",sentence_return)
+            
+            return sentence_return
+
+        if words:
+            words_list = words.split('%')
+            sentence_return = do_tokenization_from_gpt2(words_list)
+            returnstring = f"""
+            <html><body>
+            Your input: {words}
+            <hr>
+            Your answer: 
+            {sentence_return}
+            <hr>
+            </body></html>
+            """
+            return returnstring
+        else:
+            return '<html><body>Please enter `words` in the URL as a query parameter, seperated by `%` for example: `/?words=how%are%you?` </body></html>'
+    
+
 
 if __name__ == '__main__':
-    cherrypy.quickstart(HelloWorld(), '/', {'global': {'server.socket_host':'0.0.0.0','server.socket_port': 8080}})
+    cherrypy.quickstart(SayWords(), '/', {'global': {'server.socket_host':'0.0.0.0','server.socket_port': 8080}})
